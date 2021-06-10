ea47a623672150b2ab45938b81948cc64cf9c7d3
Gabriel <77230723+gycgabriel@users.noreply.github.com>
2021-06-10 16:50:45 +0800

    Delete SmoothMovement in PlayerController.

--- Shadow/Assets/Scripts/PlayerController.cs
+++ Shadow/Assets/Scripts/PlayerController.cs
@@ -174,31 +174,6 @@ public class PlayerController : Singleton<PlayerController>
         lastMove = currentMove;
     }
 
-
-    //Co-routine for moving units from one space to next, takes a parameter end to specify where to move to.
-    protected IEnumerator SmoothMovement(Vector3 end)
-    {
-        //Calculate the remaining distance to move based on the square magnitude of the difference between current position and end parameter. 
-        //Square magnitude is used instead of magnitude because it's computationally cheaper.
-        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
-
-        //While that distance is greater than a very small amount (Epsilon, almost zero):
-        while (sqrRemainingDistance > float.Epsilon)
-        {
-            //Find a new position proportionally closer to the end, based on the moveTime
-            Vector3 newPosition = Vector3.MoveTowards(myRigidBody.position, end, moveSpeed * Time.deltaTime);
-
-            //Call MovePosition on attached Rigidbody2D and move it to the calculated position.
-            myRigidBody.MovePosition(newPosition);
-
-            //Recalculate the remaining distance after moving.
-            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
-
-            //Return and loop until sqrRemainingDistance is close enough to zero to end the function
-            yield return null;
-        }
-    }
-
     public override void Destroy()
     {
         Destroy(movePoint.gameObject);
