a34df0476c071c163f7d17b4ab56c679162d8c7e
Gabriel <77230723+gycgabriel@users.noreply.github.com>
2021-06-23 20:30:51 +0800

    Trying to make ChooseCharacter Event

---
+++ Shadow/Assets/Scripts/Story/ChooseClassEvent.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseClassEvent : MonoBehaviour
+{
+    // Prefabs of all possible classes
+    public GameObject guardianPrefab;
+    public GameObject sorcererPrefab;
+
+    public ChooseCharClassUI cccUI;
+    public static string playerClass;
+    public static string shadowClass;
+
+    void Start()
+    {
+        cccUI = FindObjectOfType<ChooseCharClassUI>();
+        cccUI.gameObject.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (cccUI.confirmed && playerClass == "")
+        {
+            playerClass = cccUI.chosenClass;
+            cccUI.confirmed = false;
+            cccUI.gameObject.SetActive(false);
+            GetText.LoadChapter(0);
+            GetText.LoadScenario(1);
+            Singleton<ScenarioManager>.scriptInstance.PlayScenario(() =>
+            {
+                cccUI.gameObject.SetActive(true);
+            });
+        }
+        else if (cccUI.confirmed && shadowClass == "")
+        {
+            shadowClass = cccUI.chosenClass;
+            cccUI.confirmed = false;
+            cccUI.gameObject.SetActive(false);
+            NextScene();
+        }
+    }
+    void NextScene() 
+    {   
+        // for now
+        GameObject player = Instantiate(guardianPrefab, new Vector3(-10.5f, 3.5f, 0f), Quaternion.identity);
+        player.transform.parent = Singleton<PartyController>.gameInstance.transform;
+        player.GetComponent<Player>().chooseCharClass("Guardian");
+        GameObject shadow = Instantiate(sorcererPrefab, new Vector3(-10.5f, 3.5f, 0f), Quaternion.identity);
+        shadow.transform.parent = Singleton<PartyController>.gameInstance.transform;
+        shadow.GetComponent<Player>().chooseCharClass("Sorcerer");
+        shadow.SetActive(false);
+        SceneManager.LoadScene("hometown");
+    }
+}
