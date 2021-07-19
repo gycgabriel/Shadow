478fa2d73d201064a6b42ce006ca0e579db78358
Gabriel <77230723+gycgabriel@users.noreply.github.com>
2021-07-19 19:54:22 +0800

    Fix Flashback.
    Might want to adjust brown color in the future.

--- Shadow/Assets/Scripts/UI/Flashback.cs
+++ Shadow/Assets/Scripts/UI/Flashback.cs
@@ -21,15 +21,16 @@ public class Flashback : Singleton<Flashback>
 
     IEnumerator FadeImage(bool fadeOut, System.Action action = null)
     {
+        float fadeTime = 4f;
         float initTime = Time.realtimeSinceStartup;
         float currentTime = Time.realtimeSinceStartup;
         if (fadeOut)
         {
             // loop over 4 seconds backwards
-            for (float i = 4; i >= 0; i -= (currentTime - initTime))
+            for (float i = fadeTime; i >= 0; i -= (currentTime - initTime))
             {
                 Debug.Log("fading out");
-                img.color = new Color(img.color[0], img.color[1], img.color[2], i * initAlpha);
+                img.color = new Color(img.color[0], img.color[1], img.color[2], i/fadeTime * initAlpha);
                 currentTime = Time.realtimeSinceStartup;
                 yield return null;
             }
@@ -41,10 +42,10 @@ public class Flashback : Singleton<Flashback>
             img.color = new Color(img.color[0], img.color[1], img.color[2], 0);
             img.gameObject.SetActive(true);
             // loop over 4 seconds
-            for (float i = 0; i <= 4; i += (Time.realtimeSinceStartup - initTime))
+            for (float i = 0; i <= fadeTime; i += (Time.realtimeSinceStartup - initTime))
             {
                 Debug.Log("fading in");
-                img.color = new Color(img.color[0], img.color[1], img.color[2], i * initAlpha);
+                img.color = new Color(img.color[0], img.color[1], img.color[2], i/fadeTime * initAlpha);
                 currentTime = Time.realtimeSinceStartup;
                 yield return null;
             }
