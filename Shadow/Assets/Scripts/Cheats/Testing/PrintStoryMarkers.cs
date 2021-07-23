a59edb346cba9fdec8b83d6147f868d1740e6e48
Gabriel <77230723+gycgabriel@users.noreply.github.com>
2021-07-23 16:29:43 +0800

    Create Debug print script to debug save load.

---
+++ Shadow/Assets/Scripts/Cheats/Testing/PrintStoryMarkers.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PrintStoryMarkers : MonoBehaviour
+{
+    void Update()
+    {
+        if (StoryManager.scriptInstance != null && Input.GetKeyDown(KeyCode.RightShift))
+        {
+            Print();
+        }
+    }
+    void Print()
+    {
+        if (StoryManager.scriptInstance == null)
+            return;
+
+        string toPrint;
+
+        toPrint = "Story Evoked: ";
+        foreach (int i in StoryManager.scriptInstance.evokedStory.Keys)
+        {
+            toPrint += i + ", ";
+        }
+
+        Debug.Log(toPrint);
+
+        toPrint = "Current Quest: " + PartyController.quest.id + " " + PartyController.quest.isActive;
+
+        Debug.Log(toPrint);
+
+        toPrint = "Accepted Quests: ";
+        foreach (int qIndex in StoryManager.scriptInstance.acceptedQuests.Keys)
+        {
+            toPrint += qIndex + ", ";
+        }
+
+        Debug.Log(toPrint);
+
+        toPrint = "Completed Quests: ";
+        foreach (int qIndex in StoryManager.scriptInstance.completedQuests.Keys)
+        {
+            toPrint += qIndex + ", ";
+        }
+
+        Debug.Log(toPrint);
+
+        
+    }
+}
