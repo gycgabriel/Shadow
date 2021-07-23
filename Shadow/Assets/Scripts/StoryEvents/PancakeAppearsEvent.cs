9ecd6d8c7809345db8b13704af2e158386315002
Gabriel <77230723+gycgabriel@users.noreply.github.com>
2021-07-23 11:30:38 +0800

    Fix shadow active event bug.

--- Shadow/Assets/Scripts/StoryEvents/PancakeAppearsEvent.cs
+++ Shadow/Assets/Scripts/StoryEvents/PancakeAppearsEvent.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class PancakeAppearsEvent : MonoBehaviour
 {
+    public Quest quest3;
+
     private void Update()
     {
         if (StoryManager.scriptInstance.CheckEvoked(1, 5))
             return;
-        if (PartyController.shadowActive && StoryManager.scriptInstance.CheckEvoked(0, 4))          // tut for switch chara
+        if (StoryManager.scriptInstance.CheckAcceptedQuests(quest3) && PartyController.shadowActive && StoryManager.scriptInstance.CheckEvoked(0, 4))          // tut for switch chara
         {
             ScenarioManager.scriptInstance.PlayScenario(1, 5);
             StoryManager.scriptInstance.SetEvoked(1, 5);
