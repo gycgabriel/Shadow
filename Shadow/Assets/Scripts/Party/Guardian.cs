3bb6973d60f35cbc939418b4a177fd67ee16511c
szelongq <72333104+szelongq@users.noreply.github.com>
2021-07-25 18:22:09 +0800

    Adjust growth curves for EXP and stats.
    Reduce steepness of EXP required for level up.
    Stats growth now follow Logarithmic curve.
    Lower item prices.
    Increase Quest Gold rewards.
    Adjust damage calculation formula.

--- Shadow/Assets/Scripts/Party/Guardian.cs
+++ Shadow/Assets/Scripts/Party/Guardian.cs
@@ -11,18 +11,18 @@ public class Guardian : CharacterClass
     public Guardian()
     {
         className = "Guardian";
-        startStats = new Stats(150, 50, 50, 20, 5, 15, 20, 0);
-        expFormula = x => Mathf.FloorToInt(Mathf.Pow(x, 3f) + 14f);
+        startStats = new Stats(150, 50, 40, 60, 5, 30, 20, 0);
+        expFormula = x => Mathf.RoundToInt(Mathf.Pow(x, 2.75f) + 49 * x);
         statIncreaseFormula = new Dictionary<string, System.Func<int, int>>()
         {
-            {"hp", x =>  Mathf.FloorToInt(Mathf.Pow(x, 1.5f) + 14f) },
-            {"mp", x =>  Mathf.FloorToInt(Mathf.Pow(x, 0.5f) + 10f) },
-            {"atk", x =>  Mathf.FloorToInt(Mathf.Pow(x, 1.4f) + 12f) },
-            {"def", x =>  Mathf.FloorToInt(Mathf.Pow(x, 1.5f) + 14f) },
-            {"matk", x =>  Mathf.FloorToInt(Mathf.Pow(x, 0.5f) + 5f) },
-            {"mdef", x =>  Mathf.FloorToInt(Mathf.Pow(x, 1.25f) + 14f) },
-            {"agi", x =>  Mathf.FloorToInt(Mathf.Pow(x, 1f) + 5f) },
-            {"luk", x =>  Mathf.FloorToInt(Mathf.Pow(x, 1f) + 5f) },
+            {"hp", x =>  LogGrowthFormula(x, 75) - LogGrowthFormula(x - 1, 75) },
+            {"mp", x =>  LogGrowthFormula(x, 50) - LogGrowthFormula(x - 1, 50) },
+            {"atk", x =>  LogGrowthFormula(x, 80) - LogGrowthFormula(x - 1, 80)  },
+            {"def", x =>  LogGrowthFormula(x, 90) - LogGrowthFormula(x - 1, 90) },
+            {"matk", x =>  LogGrowthFormula(x, 0) },
+            {"mdef", x =>  LogGrowthFormula(x, 45) - LogGrowthFormula(x - 1, 45) },
+            {"agi", x =>  LogGrowthFormula(x, 0) },
+            {"luk", x =>  LogGrowthFormula(x, 0) },
         };
     }
 
