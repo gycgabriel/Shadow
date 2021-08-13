d7a84efa9c608e0cd2a22d656edce5484003d01e
Gabriel <77230723+gycgabriel@users.noreply.github.com>
2021-08-13 22:02:10 +0800

    Add game end splash screen

--- Shadow/Assets/Scripts/AfterCredits.cs
+++ Shadow/Assets/Scripts/AfterCredits.cs
@@ -5,9 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class AfterCredits : MonoBehaviour
 {
+    public Animator nextAnim;
+
     void LoadRainHouse()
     {
         FadeCanvas.scriptInstance.FadeWhiteToScene("RainHouse");
         TransferPlayer.Teleport("credits", new Vector2(0, -1));
+    }
+
+    void NextAnim()
+    {
+        nextAnim.SetBool("creditsOver", true);
     }
 }
